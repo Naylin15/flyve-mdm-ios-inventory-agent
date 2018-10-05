2b9f27dbbedef90d0a5c6155fa941f81e4c53f10
Naylin Medina <nmedina@teclib.com>
2018-10-05 11:29:36 -0400

    test(plugin): templates
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- fastlane/actions/test_report.rb
+++ fastlane/actions/test_report.rb
@@ -16,11 +16,68 @@ module Fastlane
         file = File.new(File.expand_path(params[:report_path]))
         doc = Document.new(file)
 
-        template = File.read(params[:template_path])
+        if params[:template_path] = nil
+        template = '---
+        layout: testReport
+        ---
+                        
+                        <div class="total row">
+                        <h2 class="col-sm-18">Test Results</h2>
+                        <h2 class="col-sm-6 text-right"><%= doc.root.attributes["tests"] %> tests</h2>
+                    </div>
+                    <div>
+                    <% i = 0 %>
+                        <% doc.elements.each("testsuites/testsuite") do |name| %>
+                            <% i = i + 1 %>
+                            <% if name.attributes["failures"] == "0" %>
+                        <div class="test-suite row test-suite--passing" onclick="changeDisplay(<%= "\'#test-#{i}\'"%>)">
+                            <% else %>
+                        <div class="test-suite row test-suite--failing" onclick="changeDisplay(<%= "\'#test-#{i}\'"%>)">
+                            <% end %>
+                            <h4 class="col-sm-20"><%= name.attributes["name"] %></h4>
+                            <h4 class="col-sm-4 text-right"><%= name.attributes["tests"] %> tests</h4>
+                        </div>
+                        <!-- Failing or passing class -->
+                        <div id="<%= "test-#{i}"%>">
+                         
+                            <% doc.elements.each("testsuites/testsuite/testcase") do |test| %>
+                                <% if test.attributes["classname"] == name.attributes["name"] %>
+                                    
+                                    <% if test.attributes["time"] == nil %>
+                                <div class="test-case--failing row">
+                                 <div class="col-sm-16">
+                                     <p><%= test.attributes["name"] %></p>
+                                 </div>
+                                 <div class="col-sm-4">
+                                     <p><%= test.elements["failure"].attributes["message"] %></p>
+                                 </div>
+                                 <div class="col-sm-4 text-right">
+                                     <p><%= test.elements["failure"].text %></p>
+                                 </div>
+                                </div>
+                                    <% else %>
+                                <div class="test-case--passing row">
+                                 <div class="col-sm-20">
+                                     <p><%= test.attributes["name"] %></p>
+                                 </div>
+                                 <div class="col-sm-4 text-right">
+                                     <p><%= test.attributes["time"] %></p>
+                                 </div>
+                                </div>
+                                    <% end %>
+                                <% end %>
+                            <% end %>
+                        </div>
+                         <% end %>
+                
+                    </div>'
+        else
+          template = File.read(params[:template_path])
+        end
 
         result = ERB.new(template).result(binding())
 
-        open('./fastlane/test_output/index.html', 'w') do |f|
+        open(params[:report_path], 'w') do |f|
           f.puts result
         end
 
@@ -52,7 +109,8 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :template_path,
                                   env_name: "TEMPLATE_PATH",
                                description: "Path to the template",
-                                      type: String)
+                                      type: String,
+                                  optional: true)
         ]
       end
 
